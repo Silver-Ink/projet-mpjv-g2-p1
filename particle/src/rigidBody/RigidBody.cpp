c790e55d77d1c0478ec767aa4c6cb3f02736c53b
MatthieuFontaine1 <matthieu.fontaine591@gmail.com>
2024-11-07 19:49:40 -0500

    feat: Added RigidBody draw method

--- particle/src/rigidBody/RigidBody.cpp
+++ particle/src/rigidBody/RigidBody.cpp
@@ -26,3 +26,37 @@ void RigidBody::update(float _dt)
 	up = rotationMatrix * initialUp;
 	right = rotationMatrix * initialRight;*/
 }
+
+void RigidBody::draw()
+{
+	ofPushMatrix(); // Temporarly moving pivot
+
+	ofTranslate((glm::vec3)massCenter.getPos()); // Pivot translation to box center
+
+	// Drawing box line by line
+	Vec3 boxEnd1 = front - up + right;
+
+	ofDrawLine((glm::vec3)boxEnd1, (glm::vec3)(boxEnd1 - 2 * front));
+	ofDrawLine((glm::vec3)boxEnd1, (glm::vec3)(boxEnd1 + 2 * up));
+	ofDrawLine((glm::vec3)boxEnd1, (glm::vec3)(boxEnd1 - 2 * right));
+
+	Vec3 boxEnd2 = -1 * front + up + right;
+
+	ofDrawLine((glm::vec3)boxEnd2, (glm::vec3)(boxEnd2 + 2 * front));
+	ofDrawLine((glm::vec3)boxEnd2, (glm::vec3)(boxEnd2 - 2 * up));
+	ofDrawLine((glm::vec3)boxEnd2, (glm::vec3)(boxEnd2 - 2 * right));
+
+	Vec3 boxEnd3 = -1 * front - up - right;
+
+	ofDrawLine((glm::vec3)boxEnd3, (glm::vec3)(boxEnd3 + 2 * front));
+	ofDrawLine((glm::vec3)boxEnd3, (glm::vec3)(boxEnd3 + 2 * up));
+	ofDrawLine((glm::vec3)boxEnd3, (glm::vec3)(boxEnd3 + 2 * right));
+
+	Vec3 boxEnd4 = front + up - right;
+
+	ofDrawLine((glm::vec3)boxEnd4, (glm::vec3)(boxEnd4 - 2 * front));
+	ofDrawLine((glm::vec3)boxEnd4, (glm::vec3)(boxEnd4 - 2 * up));
+	ofDrawLine((glm::vec3)boxEnd4, (glm::vec3)(boxEnd4 + 2 * right));
+
+	ofPopMatrix(); // Resetting pivot
+}
