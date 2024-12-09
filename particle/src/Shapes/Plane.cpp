982b9c55784fe9b1adeeb0fad3d8d3ee7feb1f46
Enzo <enzo.mariant@gmail.com>
2024-12-08 22:36:45 -0500

    more stability

--- particle/src/Shapes/Plane.cpp
+++ particle/src/Shapes/Plane.cpp
@@ -47,14 +47,14 @@ void Plane::draw()
 	ofDrawPlane(origin.x,-origin.z,-origin.y, size, size);
 	ofPopMatrix();
 }
-
 void Plane::collisionResolve(RigidBody* _rb)
 {
     array<Vec3, 8> points;
     _rb->getPoints(points);
     Vec3 correction(0, 0, 0);
-    Vec3 totalImpulsion(0, 0, 0);
+    Vec3 totalTorque(0, 0, 0);
     Vec3 averagePoint(0, 0, 0);
+	Vec3 totalImpulse(0, 0, 0);
     int collisionCount = 0;
 
     for (Vec3 point : points)
@@ -68,27 +68,34 @@ void Plane::collisionResolve(RigidBody* _rb)
             averagePoint += projectedPoint;
             collisionCount++;
 
-            // Calculer l'impulsion en tenant compte de la masse infinie du plan
-            float massCube = _rb->massCenter.getMass();
-            Vec3 impulsion = -massCube * normal;
-            totalImpulsion += impulsion;
+
+			totalImpulse += normal * impulsionStrength;
+
+            Vec3 r = point - _rb->massCenter.getPos();
+            Vec3 force = -normal;
+            Vec3 torque = r.cross(force);
+            totalTorque += torque;
         }
     }
 
     if (collisionCount > 0)
     {
-        correction /= collisionCount; // Moyenne des corrections
-        averagePoint /= collisionCount; // Moyenne des points d'application
+        correction /= collisionCount; 
+        averagePoint /= collisionCount; 
         _rb->massCenter.getPos() -= correction;
 
-        // Appliquer l'impulsion totale au point moyen
-        _rb->massCenter.addImpulsion(totalImpulsion/2, averagePoint);
+        // Appliquer le couple total
+        _rb->massCenter.addImpulsion(-totalImpulse/2, averagePoint); // Pas de force, juste pour le point d'application
+        _rb->massCenter.accumTorque += totalTorque;
 
         // Ajuster la vélocité pour éviter les oscillations
         Vec3 velocity = _rb->massCenter.getVelocity();
-        if (velocity.dot(correction) < 0)
+        float dotProduct = velocity.dot(correction);
+        if (dotProduct < 0)
         {
-            _rb->massCenter.getVelocity() -= velocity.dot(correction) * correction.getNormalized();
+            // Augmenter l'effet de la correction en multipliant par un facteur plus grand
+            float attenuationFactor = 2.0f; // Vous pouvez ajuster ce facteur pour plus d'atténuation
+            _rb->massCenter.getVelocity() -= attenuationFactor * dotProduct * correction.getNormalized();
         }
     }
 }
